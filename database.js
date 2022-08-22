e61325aa133f4d9e6e6c6cd8d822e1447dfc242a
Silva <wesley.santos.silva@ccc.ufcg.edu.br>
2022-08-22 20:34:55 -0300

    feat: add database

---
+++ database.js
@@ -0,0 +1,40 @@
+const books = [
+  {
+    id: 1,
+    name: "Homens pretos (não) choram",
+    author: "Stefano Volp",
+    category: "Contos",
+    pages: 213,
+    recommmend: true,
+    read: true
+  },
+  {
+    id: 2,
+    name: "Teoria da Música",
+    author: "Bohumil Med",
+    category: "Música",
+    pages: 423,
+    recommmend: true,
+    read: false
+  },
+  {
+    id: 3,
+    name: "O nome do Vento",
+    author: "Patrick Rothfuss",
+    category: "Fantasia Épica",
+    pages: 656,
+    recommmend: true,
+    read: true
+  },
+  {
+    id: 4,
+    name: "As veias abertas da América Latina",
+    author: "Eduardo Galeano",
+    category: "História",
+    pages: 391,
+    recommmend: true,
+    read: false
+  }
+]
+
+module.export = books;
